81cf4bd5b029b3faf153d6149b3a8edde1cc015e
ezioraciti <ezio-17@hotmail.it>
2020-03-26 00:59:22 +0100

    fix porta per il server

--- esercizi/microservices/microservice-Hsl2Hex/src/controller/HttpController.componentTest.ts
+++ esercizi/microservices/microservice-Hsl2Hex/src/controller/HttpController.componentTest.ts
@@ -9,7 +9,7 @@ chai.use(chaiHttp);
 
 //TODO --FATTO
 describe('REST API test suite description', () => {
-    const url = process.env.npm_config_rgb2hex_test_url || `http://localhost:hsl2hex`;
+    const url = process.env.npm_config_rgb2hex_test_url || `http://localhost:${config.port}`;
     console.log('Test URL: ' + url);
 
     hsl2hexTestData.forEach((test) => {
